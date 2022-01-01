b97d7be4a5ac79bd2cf59be77af1be940394725d
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-01-01 15:33:41 -0600

    Initial commit

--- tapper/ContentView.swift
+++ tapper/ContentView.swift
@@ -7,10 +7,41 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var taps: Int = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            ZStack {
+                Color.purple
+                    .ignoresSafeArea()
+            VStack {
+                
+                Spacer()
+                Label("\(taps)", systemImage: "hand.tap.fill")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                    .border(.yellow, width: 7)
+                    .cornerRadius(7)
+                Spacer()
+                Button (action: {
+                    taps += 1                }, label:
+                {  Image(systemName: "hammer.circle")
+                        .resizable()
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.yellow, .white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.width * 0.4)
+                        .shadow(color: .black, radius: 5, x: 0, y: 5)
+
+                })
+                Spacer()
+            }
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+            }
+        }
     }
 }
 
