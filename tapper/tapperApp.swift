ca46ec93a137d9e20d060c0100b967b364c8cef5
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-01-01 12:55:47 -0600

    Initial Commit

---
+++ tapper/tapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  tapperApp.swift
+//  tapper
+//
+//  Created by Geoffrey Johnson on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct tapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
